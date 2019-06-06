4088fe7540f049014c1deec0893864161d927ae7
wangyk2019 <627245987@qq.com>
2019-06-06 13:06:56 +0800

    test2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!我来了");
+		System.out.println("Hello World!我来了1");
 	}
 
 }
